4100ce91a8402960d4588e74f65da75e4c33edfa
Roee FL <roeefl@gmail.com>
2017-01-02 01:42:08 +0200

    1
    1

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <hr/>
+    <img src="./assets/logo.png">
+    <h1>I am the App component</h1>
+    <h3>I show the current value in formData.value: {{formData.value}}</h3>
+    <hello :formData='formData'></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      formData: {
+        name: 'Address Form',
+        value: 'Area 51'
+      }
+    }
+  },
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <hr/>
+    <div class="form-group">
+      <h1>I am the Hello component</h1>
+      <h2>{{formData.name}}</h2>
+      <input type="text" class="form-control" v-model="formData.value" :name="formData.name">
+    </div>
+    <hr/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    props: ['formData'],
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+    color: red;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
+
